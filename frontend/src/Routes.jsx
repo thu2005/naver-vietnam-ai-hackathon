a35bbf697508c2e981062cfd6d6db8e448ad5489
ynnhi2607 <huynhyennhi2607@gmail.com>
2025-11-10 13:35:02 +0700

    feat: add routine recommendations page

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import ScrollToTop from "components/ScrollToTop";
 import ErrorBoundary from "components/ErrorBoundary";
 import Home from "./pages/Home";
+import RoutineRecommendations from "./pages/routine-recommendations";
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
         <ScrollToTop />
         <RouterRoutes>
           <Route path="/" element={<Home />} />
+          <Route path="routine" element={<RoutineRecommendations />} />
         </RouterRoutes>
       </ErrorBoundary>
     </BrowserRouter>
