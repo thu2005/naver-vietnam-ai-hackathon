ce511312453ad9f5ff4f745c06b5612a9a5e3dbd
Ha Bao Ngoc <hiembaonon@gmail.com>
2025-11-14 01:14:18 +0700

    add: product controller and routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import rateLimit from 'express-rate-limit';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js';
 import weatherRoutes from './routes/weather.route.js'
+import productRoutes from './routes/product.route.js';
 
 const app = express();
 
@@ -34,6 +35,7 @@ app.use(morgan('dev'));
 
 // Routes
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 app.use('/api/weather', weatherRoutes);
 
 // app.use('/api/routines', routineRoutes); // TODO: Create routine routes
